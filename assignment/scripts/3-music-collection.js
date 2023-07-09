15110434999a3271b7034ca1faf19348952a7016
Fraol Ayehu <phrolaye@gmail.com>
2023-07-09 17:30:09 -0500

    added albums

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -14,3 +14,12 @@ function addToCollection(title, artist, yearPublished) {
 
   return album;
 }
+
+console.log(addToCollection("What's Going On", "Marvin Gaye", 1971));
+console.log(addToCollection("Love Trip", "Takako Mamiya", 1982));
+console.log(addToCollection("Die Lit", "Playboi Carti", 2018));
+console.log(addToCollection("Channel Orange", "Frank Ocean", 2012));
+console.log(addToCollection("Anything in Return", "Toro y Moi", 2013));
+console.log(addToCollection("Love Deluxe", "Sade", 1992));
+
+console.log(collection);

54d097ab2724cc9fd457ec247fa23dfadf3bb924
Fraol Ayehu <phrolaye@gmail.com>
2023-07-09 17:24:36 -0500

    array and function added

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,16 @@
 console.log('***** Music Collection *****')
+
+const collection = [];
+
+function addToCollection(title, artist, yearPublished) {
+    const album = {
+      title: title,
+      artist: artist,
+      yearPublished: yearPublished
+    };
+
+
+  collection.push(album);
+
+  return album;
+}
